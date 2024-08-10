5ff195f826903e0b2f8443ac99d4f9f92d6f648b
AsmaraHassan <161068776+AsmaraHassan@users.noreply.github.com>
2024-08-10 04:48:55 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Problem No 1
+// write a function called greetuser
+// function greetUser(){
+//     console.log(" Hello! Welcome to the class  ")
+// }
+// greetUser()
+// Problem no 2
+// Write a program sum of two numbers
+// function sumoftwonumbers(a,b)
+// {
+// console.log(a+b)
+
+// }
+// sumoftwonumbers(3,7)
+
+// Program no 3
+// write a program  (Create a function name square displa)
+// function displaysquare(num)
+// {
+
+//     console.log(num*num)
+// }
+// displaysquare(5)
